563c2bf57668b4f3c227b4dd7a4162c543723f6e
Evan Wright <evanwright@Beared-Book.local>
2020-02-23 23:18:50 -0700

    added javaScript

--- javascript/quickshots.js
+++ javascript/quickshots.js
@@ -3,13 +3,14 @@ const quickShot = [document.getElementsByClassName('dronie'), document.getElemen
 function showAll() {
    let all = quickShot.filter(quickShot => quickShot);
 
-   return all; 
+   console.log(all); 
 }
 //console.log(showAll());
 
 function showDronie() {
-    let dronie = quickShot.filter(quickShot => quickShot[0])
+    let dronie = quickShot.filter(quickShot => quickShot[0]);
 
+    console.log(dronie);
     return dronie;
 }
 console.log(showDronie())
