cd0a4870c7a5734868ea8a090ad750e3f0093c60
Evan Wright <evanwright@Beared-Book.local>
2020-02-24 20:06:28 -0700

    updated quickshots.js

--- javascript/quickshots.js
+++ javascript/quickshots.js
@@ -17,24 +17,72 @@
 
 // console.log(quickShot);
 
-const quickShot = [{
-    video: document.getElementsByClassName('dronie'),
-    type: 'dronie' 
-},
-{
-    video: document.getElementsByClassName('rocket'),
-    type: 'rocket'
-},
-{
-    video: document.getElementsByClassName('helix'),
-    type: 'helix'
-}];
+// const quickShot = [{
+//     video: './quickshots/bridalVeilFalls.mov',
+//     name: 'Bridal Veil Falls',
+//     type: 'dronie' 
+// },
+// {
+//     video: './quickshots/provoCanyon.mov',
+//     name: 'Provo Canyon',
+//     type: 'dronie'
+// },
+// {
+//     video: document.getElementsByClassName('rocket'),
+//     type: 'rocket'
+// },
+// {
+//     video: document.getElementsByClassName('helix'),
+//     type: 'helix'
+// }];
 
-function showDronie() {
-    let dronieShow = quickShot.filter(function(dronie) {
-    return dronie.type === 'dronie';
-})
-    console.log(dronieShow);
-};
+// function showDronie() {
+//     let dronieShow = quickShot.filter(function(dronie) {
+//     return dronie.type === 'dronie';
+// })
+//     return dronieShow;
+// };
 
-//console.log(showDronie());
+// console.log(showDronie());
+
+
+filterSelection('all')
+function filterSelection(c) {
+    let x, i;
+    x = document.getElementsByClassName('filter');
+    if (c == 'all') c = '';
+    for (i = 0; i < x.length; i++) {
+        removeClass(x[i], 'show');
+        if (x[i].className.indexOf(c) > -1) addClass(x[i], 'show');
+    }
+}
+
+function addClass(element, name) {
+    let i, arr1, arr2;
+    arr1 = element.className.split(' ');
+    arr2 = name.split(' ');
+    for (i = 0; i < arr2.length; i++) {
+        if (arr1.indexOf(arr2[i]) == -1) {element.className += ' ' + arr2[i];}
+    }
+}
+
+function removeClass(element, name) {
+    let i, arr1, arr2;
+    arr1 = element.className.split(' ');
+    arr2 = name.split(' ');
+    for (i = 0; i < arr2.length; i++) {
+        while (arr1.indexOf(arr2[i])> -1) {arr1.splice(arr1.indexOf(arr2[i]), 1);
+        }
+    }
+    element.className = arr1.join(' ');
+}
+
+var btnContainer = document.getElementById("filterBtn");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
