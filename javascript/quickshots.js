b064d9d9b35fc64443fbabb3267be59e72ba20a9
Evan Wright <evanwright@Beared-Book.local>
2020-02-24 18:40:17 -0700

    updated quichots.js

--- javascript/quickshots.js
+++ javascript/quickshots.js
@@ -1,18 +1,37 @@
-const quickShot = [document.getElementsByClassName('dronie'), document.getElementsByClassName('rocket'), document.getElementsByClassName('helix')];
+// const quickShot = [document.getElementsByClassName('dronie'), document.getElementsByClassName('rocket'), document.getElementsByClassName('helix')];
 
-function showAll() {
-   let all = quickShot.filter(quickShot => quickShot);
+// function showAll() {
+//    let all = quickShot.filter(quickShot => quickShot);
 
-   console.log(all); 
-}
-//console.log(showAll());
+//    console.log(all); 
+// }
+// //console.log(showAll());
 
-function showDronie() {
-    let dronie = quickShot.filter(quickShot => quickShot[0]);
+// function showDronie() {
+//     let dronie = quickShot.filter(quickShot => quickShot[0]);
 
-    console.log(dronie);
-    return dronie;
-}
-console.log(showDronie())
+//     console.log(dronie);
+//     return dronie;
+// }
+// console.log(showDronie())
 
-console.log(quickShot);
+// console.log(quickShot);
+
+const quickShot = [{
+    video: document.getElementsByClassName('dronie'),
+    type: 'dronie' 
+},
+{
+    video: document.getElementsByClassName('rocket'),
+    type: 'rocket'
+},
+{
+    video: document.getElementsByClassName('helix'),
+    type: 'helix'
+}];
+
+    let showDronie = quickShot.filter(function(dronie) {
+    return dronie.type === 'dronie';
+});
+
+console.log(showDronie);
